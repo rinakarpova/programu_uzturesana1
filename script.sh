077b0191f21a87d0fd5762135313b8523dd14c92
Rina Karpova <kandela@inbox.lv>
2026-01-17 21:52:14 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+vards="Dina"
+uzvards="Cherniakova"
+vecums=25
+datums=$(date +"%d-%m-%Y")
+
+
+echo "Hello, you are " $vards $uzvards
+sleep 2
+echo "Today is " $datums "and you are" $vecums "years!"
